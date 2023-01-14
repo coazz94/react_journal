5347469a6b0f23381a2d0f944773776e0c59f62c
coazz <aco94@hotmail.com>
2023-01-14 16:44:12 +0100

    upload

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import "./components.css"
+
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <h2>my travel journal</h2>
+        </nav>
+    );
+}
+
+
+export {Navbar};
